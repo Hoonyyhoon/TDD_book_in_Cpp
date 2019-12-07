e4d5e9c7e1976cf38c1568587b3325162c9c02f9
JunghoonKim <jhkim@jmarple.ai>
2019-12-07 20:39:26 +0900

    Proceed chapter4: Privacy

--- src/part1/money_example.cpp
+++ src/part1/money_example.cpp
@@ -1,18 +1,10 @@
 #include <assert.h>
-#include <memory>
-#include <iostream>
 #include <gtest/gtest.h>
+#include <iostream>
+#include <memory>
 
 /*
 Done:
-    1) 5+10chf = $10
-O   2) 5*2 = 10
-    3) make "amount" private
-O   4) side effect of dollar?
-    5) money rounding?
-    6) equals()
-
-To do:
     1) 5+10chf = $10
 O   2) 5*2 = 10
     3) make "amount" private
@@ -28,48 +20,66 @@ O   6) equals()
     - Implemented it simply
     - Didn't refactor immediately, but instead tested further
     - Refactored to capture the two cases at once
+
+To do:
+    1) 5+10chf = $10
+O   2) 5*2 = 10
+O   3) make "amount" private
+O   4) side effect of dollar?
+    5) money rounding?
+O   6) equals()
+    7) HashCode()
+    8) Equal null
+    9) Equal object
+
+    - Used functionality just developed to improve a test
+    - Noticed that if two tests fail at once we're sunk
+    - Proceeded in spite of the risk
+    - Used new functionality in the object under test to reduce coupling between
+the tests and the code
 */
 
-class Dollar { 
-    public:
-        Dollar(double amount) : amount(amount){;}
-        double amount;
+class Dollar {
+ private:
+  double amount;
 
-        std::shared_ptr<Dollar> times(int multiplier) {
-            return std::make_shared<Dollar>( amount * multiplier );
-        }
+ public:
+  Dollar(double amount) : amount(amount) { ; }
 
-        bool equals(const std::shared_ptr<Dollar> comp) {
-            return amount == comp->amount;
-        }
+  std::shared_ptr<Dollar> times(const int multiplier) {
+    return std::make_shared<Dollar>(amount * multiplier);
+  }
+
+  bool equals(const std::shared_ptr<Dollar> comp) {
+    return amount == comp->amount;
+  }
 };
 
-// void testMultiplication() {
-//     std::shared_ptr<Dollar> five = std::make_shared<Dollar>(5);
-//     five->times(2);
-//     assert(10 == five->amount);
-// }
+TEST(MoneyExample, multiplication) {
+  std::shared_ptr<Dollar> five = std::make_shared<Dollar>(5);
+  // override EXPECT_EQ?
+  // EXPECT_EQ(five->times(2), std::make_shared<Dollar>(10))
+  // << "The amount is not correct";
+  // EXPECT_EQ(five->times(3), std::make_shared<Dollar>(15))
+  // << "The amount is not correct";
 
-//our first unit test
-TEST(MoneyExample, multiplication)
-{
-    std::shared_ptr<Dollar> five = std::make_shared<Dollar>(5);
-    std::shared_ptr<Dollar> product = five->times(2);
-    EXPECT_EQ(product->amount, 10) << "The amount is not correct";
-    product = five->times(3);
-    EXPECT_EQ(product->amount, 15) << "The amount is not correct";
+  // !! Risk: test for eqality and multiplication both depends on function
+  // equals
+  EXPECT_TRUE(five->times(2)->equals(std::make_shared<Dollar>(10)))
+      << "The amount is not correct";
+  EXPECT_TRUE(five->times(3)->equals(std::make_shared<Dollar>(15)))
+      << "The amount is not correct";
 }
 
 TEST(MoneyExample, equality) {
-    std::shared_ptr<Dollar> five = std::make_shared<Dollar>(5);
-    
-    // triangulation
-    EXPECT_TRUE( five->equals( std::make_shared<Dollar>(5) ) ) << " Same amount";
-    EXPECT_FALSE( five->equals( std::make_shared<Dollar>(6) ) )<<" Not a same";
+  std::shared_ptr<Dollar> five = std::make_shared<Dollar>(5);
+
+  // triangulation
+  EXPECT_TRUE(five->equals(std::make_shared<Dollar>(5))) << " Same amount";
+  EXPECT_FALSE(five->equals(std::make_shared<Dollar>(6))) << " Not a same";
 }
 
-int main(int argc, char **argv) {
-  testing::InitGoogleTest( &argc, argv );
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-
