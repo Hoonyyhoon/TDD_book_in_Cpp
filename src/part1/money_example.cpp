8a5efa72e7e06cc43118747582862ad2b3db8ad6
JunghoonKim <jhkim@jmarple.ai>
2019-12-07 20:56:21 +0900

    Proceed chapter5: Franc-ly Speaking

--- src/part1/money_example.cpp
+++ src/part1/money_example.cpp
@@ -7,23 +7,6 @@
 Done:
     1) 5+10chf = $10
 O   2) 5*2 = 10
-    3) make "amount" private
-O   4) side effect of dollar?
-    5) money rounding?
-O   6) equals()
-    7) HashCode()
-    8) Equal null
-    9) Equal object
-
-    - Noticed that our design pattern (Value Object) implied an operation
-    - Tested for that operation
-    - Implemented it simply
-    - Didn't refactor immediately, but instead tested further
-    - Refactored to capture the two cases at once
-
-To do:
-    1) 5+10chf = $10
-O   2) 5*2 = 10
 O   3) make "amount" private
 O   4) side effect of dollar?
     5) money rounding?
@@ -31,12 +14,19 @@ O   6) equals()
     7) HashCode()
     8) Equal null
     9) Equal object
+    10) 5CHF * 2 = 10 CHF (multi currency)
+    11) Dollar/Franc duplication
+    12) Common equals
+    13) Common times
 
-    - Used functionality just developed to improve a test
-    - Noticed that if two tests fail at once we're sunk
-    - Proceeded in spite of the risk
-    - Used new functionality in the object under test to reduce coupling between
-the tests and the code
+    - Couldn’t tackle a big test, so we invented a small test that represented
+progress
+    - Wrote the test by shamelessly duplicating and editing
+    - Even worse, made the test work by copying and editing model code wholesale
+    - Promised ourselves we wouldn’t go home until the duplication was gone
+
+To do:
+
 */
 
 class Dollar {
@@ -50,33 +40,57 @@ class Dollar {
     return std::make_shared<Dollar>(amount * multiplier);
   }
 
-  bool equals(const std::shared_ptr<Dollar> comp) {
+  template <typename T>
+  bool equals(const std::shared_ptr<T> comp) {
     return amount == comp->amount;
   }
 };
 
-TEST(MoneyExample, multiplication) {
+class Franc {
+ private:
+  double amount;
+
+ public:
+  Franc(double amount) : amount(amount) { ; }
+
+  std::shared_ptr<Franc> times(const int multiplier) {
+    return std::make_shared<Franc>(amount * multiplier);
+  }
+
+  template <typename T>
+  bool equals(const std::shared_ptr<T> comp) {
+    return amount == comp->amount;
+  }
+};
+
+TEST(MoneyExample, Multiplication) {
   std::shared_ptr<Dollar> five = std::make_shared<Dollar>(5);
-  // override EXPECT_EQ?
-  // EXPECT_EQ(five->times(2), std::make_shared<Dollar>(10))
-  // << "The amount is not correct";
-  // EXPECT_EQ(five->times(3), std::make_shared<Dollar>(15))
-  // << "The amount is not correct";
 
   // !! Risk: test for eqality and multiplication both depends on function
   // equals
-  EXPECT_TRUE(five->times(2)->equals(std::make_shared<Dollar>(10)))
+  EXPECT_TRUE(five->times(2)->equals<Dollar>(std::make_shared<Dollar>(10)))
       << "The amount is not correct";
-  EXPECT_TRUE(five->times(3)->equals(std::make_shared<Dollar>(15)))
+  EXPECT_TRUE(five->times(3)->equals<Dollar>(std::make_shared<Dollar>(15)))
       << "The amount is not correct";
 }
 
-TEST(MoneyExample, equality) {
+TEST(MoneyExample, Equality) {
   std::shared_ptr<Dollar> five = std::make_shared<Dollar>(5);
 
   // triangulation
-  EXPECT_TRUE(five->equals(std::make_shared<Dollar>(5))) << " Same amount";
-  EXPECT_FALSE(five->equals(std::make_shared<Dollar>(6))) << " Not a same";
+  EXPECT_TRUE(five->equals<Dollar>(std::make_shared<Dollar>(5)))
+      << " Same amount";
+  EXPECT_FALSE(five->equals<Dollar>(std::make_shared<Dollar>(6)))
+      << " Not a same";
+}
+
+TEST(MoneyExample, Franc_Multiplication) {
+  std::shared_ptr<Franc> five = std::make_shared<Franc>(5);
+
+  EXPECT_TRUE(five->times(2)->equals<Franc>(std::make_shared<Franc>(10)))
+      << "The amount is not correct";
+  EXPECT_TRUE(five->times(3)->equals<Franc>(std::make_shared<Franc>(15)))
+      << "The amount is not correct";
 }
 
 int main(int argc, char** argv) {
