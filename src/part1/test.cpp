fafb03f1a25953599d186e71b10b6df41c7f572e
JunghoonKim <jhkim@jmarple.ai>
2019-12-08 21:16:53 +0900

    Chapter8: Makin' Objects
    1) Several changes due to difference between cpp and java
    especially,
      1. To work with smart_pointer with covariants.
      2. To apply factory method

--- src/part1/test.cpp
+++ src/part1/test.cpp
@@ -1,36 +1,42 @@
-#include "money.h"
 #include <gtest/gtest.h>
+#include "money.h"
 
 TEST(MoneyExample, Multiplication) {
-  std::shared_ptr<Dollar> five = std::make_shared<Dollar>(5);
+  MoneyFactory mf;
+  std::unique_ptr<Money> five = mf.money(MoneyFactory::USD, 5);
 
   // !! Risk: test for eqality and multiplication both depends on function
   // equals
-  EXPECT_TRUE(five->times(2)->equals(std::make_shared<Dollar>(10)))
+  EXPECT_TRUE(five->times(2)->equals(mf.money(MoneyFactory::USD, 10)))
       << "The amount is not correct";
-  EXPECT_TRUE(five->times(3)->equals(std::make_shared<Dollar>(15)))
+  EXPECT_TRUE(five->times(3)->equals(mf.money(MoneyFactory::USD, 15)))
       << "The amount is not correct";
 }
 
 TEST(MoneyExample, Equality) {
+MoneyFactory mf;
+  std::unique_ptr<Money> five = mf.money(MoneyFactory::USD, 5);
+
   // triangulation
-  EXPECT_TRUE(std::make_shared<Dollar>(5)->equals(std::make_shared<Dollar>(5)))
+  EXPECT_TRUE( mf.money(MoneyFactory::USD, 5)->equals(mf.money(MoneyFactory::USD, 5)))
       << "Equals failed";
-  EXPECT_FALSE(std::make_shared<Dollar>(5)->equals(std::make_shared<Dollar>(6)))
+  EXPECT_FALSE(mf.money(MoneyFactory::USD, 5)->equals(mf.money(MoneyFactory::USD, 6)))
       << "Equals failed";
-  EXPECT_TRUE(std::make_shared<Franc>(5)->equals(std::make_shared<Franc>(5)))
+  EXPECT_TRUE(mf.money(MoneyFactory::CHF, 5)->equals(mf.money(MoneyFactory::CHF, 5)))
       << "Equals failed";
-  EXPECT_FALSE(std::make_shared<Franc>(5)->equals(std::make_shared<Franc>(6)))
+  EXPECT_FALSE(mf.money(MoneyFactory::CHF, 5)->equals(mf.money(MoneyFactory::CHF, 6)))
       << "Equals failed";
-  EXPECT_FALSE(std::make_shared<Franc>(5)->equals(std::make_shared<Dollar>(5))) 
+  EXPECT_FALSE(mf.money(MoneyFactory::CHF, 5)->equals(mf.money(MoneyFactory::USD, 5)))
       << "Franc, Dollars shouldn't be equal";
 }
 
 TEST(MoneyExample, Franc_Multiplication) {
-  std::shared_ptr<Franc> five = std::make_shared<Franc>(5);
-  EXPECT_TRUE(five->times(2)->equals(std::make_shared<Franc>(10)))
+  MoneyFactory mf;
+  std::unique_ptr<Money> five = mf.money(MoneyFactory::CHF, 5);
+
+  EXPECT_TRUE(five->times(2)->equals(mf.money(MoneyFactory::CHF, 10)))
       << "The amount is not correct";
-  EXPECT_TRUE(five->times(3)->equals(std::make_shared<Franc>(15)))
+  EXPECT_TRUE(five->times(3)->equals(mf.money(MoneyFactory::CHF, 15)))
       << "The amount is not correct";
 }
 
