b25a3cd3f1a22bf17fa1d367e66266f0d6523f4e
hoonyyhoon <hoonyyhoon@snu.ac.kr>
2019-12-16 00:59:27 +0900

    Ch10: Interesting Times

--- src/part1/include/money.h
+++ src/part1/include/money.h
@@ -6,18 +6,9 @@
 class Money {
  protected:
   double amount_;
-  std::string type_name_;  // store typeid name()
   std::string currency_;
 
-  // Somewhat struggling to fit in with the book using smart pointer
-  // https://stackoverflow.com/questions/6924754/return-type-covariance-with-smart-pointers
-  // https://arne-mertz.de/2016/05/covariant-smart-pointers/
  private:
-  // another layer of indirection for covariants(not so fancy)
-  // 1) called times in the base class
-  // 2) doDetour called implemented in each derived class
-  // 3) times called implemented in each derived class
-  virtual std::unique_ptr<Money> doDetour(int amount) const = 0;
 
  public:
   Money(double amount, std::string currency) {
@@ -32,49 +23,21 @@ class Money {
     return amount_ == money->amount_ && currency_ == money->getCurrency();
   }
 
-  // 1)
   std::unique_ptr<Money> times(const int multiplier) const {
-    return doDetour(multiplier);
+    return std::make_unique<Money>( multiplier * amount_, currency_ );
   }
 };
 
 class Dollar : public Money {
  private:
-  // 2)
-  virtual std::unique_ptr<Money> doDetour(int multiplier) const final override {
-    return times(multiplier);
-  }
 
- public:
+  public:
   Dollar(double amount, std::string currency) : Money(amount, currency) { ; }
-
-  // 3)
-  std::unique_ptr<Dollar> times(const int multiplier) const {
-    return std::make_unique<Dollar>(amount_ * multiplier, currency_);
-  }
 };
 
 class Franc : public Money {
  private:
-  // 2)
-  virtual std::unique_ptr<Money> doDetour(int multiplier) const final override {
-    return times(multiplier);
-  }
 
  public:
   Franc(double amount, std::string currency) : Money(amount, currency) { ; }
-
-  // 3)
-  std::unique_ptr<Franc> times(const int multiplier) const {
-    return std::make_unique<Franc>(amount_ * multiplier, currency_);
-  }
-};
-
-class MoneyFactory {
-  public:
-    static std::unique_ptr<Money> money(std::string mtype, int amount_) {
-      if (mtype == "USD") return std::make_unique<Dollar>(amount_, mtype);
-      else if ( mtype=="CHF" ) return std::make_unique<Franc>(amount_, mtype);
-      else return nullptr;
-    }
 };
